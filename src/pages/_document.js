fdc14282f7bd38c45da6c7affca28c3bca66375b
Jamie McKinnerney <18506997+himeylo@users.noreply.github.com>
2022-04-20 13:55:15 -0500

    Update for fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700;800;900&family=Roboto:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
